063200cecc514f379fe7e39f8962ee66f7d5bdd4
John T. Wodder II <git@varonathe.org>
2025-07-06 15:16:05 -0400

    OPER

--- crates/irctext/src/clientmsgs/oper.rs
+++ crates/irctext/src/clientmsgs/oper.rs
@@ -1,18 +1,40 @@
 use super::{ClientMessage, ClientMessageError, ClientMessageParts};
-use crate::{Message, ParameterList, RawMessage, ToIrcLine, Verb};
+use crate::{FinalParam, MedialParam, Message, ParameterList, RawMessage, ToIrcLine, Verb};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Oper;
+pub struct Oper {
+    name: MedialParam,
+    password: FinalParam,
+}
+
+impl Oper {
+    pub fn new(name: MedialParam, password: FinalParam) -> Oper {
+        Oper { name, password }
+    }
+
+    pub fn name(&self) -> &MedialParam {
+        &self.name
+    }
+
+    pub fn password(&self) -> &FinalParam {
+        &self.password
+    }
+}
 
 impl ClientMessageParts for Oper {
     fn into_parts(self) -> (Verb, ParameterList) {
-        todo!()
+        (
+            Verb::Oper,
+            ParameterList::builder()
+                .with_medial(self.name)
+                .with_final(self.password),
+        )
     }
 }
 
 impl ToIrcLine for Oper {
     fn to_irc_line(&self) -> String {
-        todo!()
+        format!("OPER {} :{}", self.name, self.password)
     }
 }
 
@@ -32,6 +54,7 @@ impl TryFrom<ParameterList> for Oper {
     type Error = ClientMessageError;
 
     fn try_from(params: ParameterList) -> Result<Oper, ClientMessageError> {
-        todo!()
+        let (name, password) = params.try_into()?;
+        Ok(Oper { name, password })
     }
 }

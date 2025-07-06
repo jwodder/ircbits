25b270315b6b60c5138bc061e36ba95fb757c346
John T. Wodder II <git@varonathe.org>
2025-07-06 18:37:46 -0400

    Update deps

--- crates/ircnet/src/linestream.rs
+++ crates/ircnet/src/linestream.rs
@@ -1,8 +1,9 @@
 use crate::client::MAX_LINE_LENGTH;
 use crate::codec::IrcLinesCodec;
-use anyhow::Context;
-use rustls_pki_types::ServerName;
+use itertools::Itertools; // join
+use rustls_pki_types::{InvalidDnsNameError, ServerName};
 use std::sync::Arc;
+use thiserror::Error;
 use tokio::net::TcpStream;
 use tokio_rustls::{
     rustls::{ClientConfig, RootCertStore},
@@ -14,11 +15,25 @@ pub type IrcLineStream = Framed<Either<TcpStream, TlsStream>, IrcLinesCodec>;
 
 pub type TlsStream = tokio_rustls::client::TlsStream<TcpStream>;
 
-pub async fn connect(server: &str, port: u16, tls: bool) -> anyhow::Result<IrcLineStream> {
+#[derive(Debug, Error)]
+pub enum ConnectionError {
+    #[error("failed to connect to server")]
+    Connect(#[source] std::io::Error),
+    #[error("failed to load system certificates: {0}")]
+    LoadStore(String),
+    #[error("failed to add certificates from system store: all {bad} certs were invalid")]
+    AddCerts { bad: usize },
+    #[error("invalid TLS server name")]
+    ServerName(#[from] InvalidDnsNameError),
+    #[error("failed to establish TLS connection")]
+    TlsConnect(#[source] std::io::Error),
+}
+
+pub async fn connect(server: &str, port: u16, tls: bool) -> Result<IrcLineStream, ConnectionError> {
     log::trace!("Connecting to {server:?} on port {port} ...");
     let conn = TcpStream::connect((server, port))
         .await
-        .context("Error connecting to server")?;
+        .map_err(ConnectionError::Connect)?;
     log::trace!(
         "Connected to {}",
         conn.peer_addr().map_or_else(
@@ -28,26 +43,26 @@ pub async fn connect(server: &str, port: u16, tls: bool) -> anyhow::Result<IrcLi
     );
     let conn = if tls {
         log::trace!("Initializing TLS ...");
+        let certs = rustls_native_certs::load_native_certs();
+        if !certs.errors.is_empty() {
+            let msg = certs.errors.into_iter().map(|e| e.to_string()).join("; ");
+            return Err(ConnectionError::LoadStore(msg));
+        }
+
         let mut root_cert_store = RootCertStore::empty();
-        let system_certs = rustls_native_certs::load_native_certs()
-            .context("Failed to load system certificate store")?;
-        let (good, bad) = root_cert_store.add_parsable_certificates(system_certs);
+        let (good, bad) = root_cert_store.add_parsable_certificates(certs.certs);
         if good == 0 {
-            anyhow::bail!(
-                "Failed to load any certificates from system store: all {bad} certs were invalid"
-            );
+            return Err(ConnectionError::AddCerts { bad });
         }
         let config = ClientConfig::builder()
             .with_root_certificates(root_cert_store)
             .with_no_client_auth();
         let connector = TlsConnector::from(Arc::new(config));
-        let dnsname = ServerName::try_from(server)
-            .context("Invalid TLS server name")?
-            .to_owned();
+        let dnsname = ServerName::try_from(server)?.to_owned();
         let tls_conn = connector
             .connect(dnsname, conn)
             .await
-            .context("Error establishing TLS connection")?;
+            .map_err(ConnectionError::TlsConnect)?;
         log::trace!("TLS established");
         Either::Right(tls_conn)
     } else {

27d611e99284edc48e53ea12332fb7278525e28c
John T. Wodder II <git@varonathe.org>
2025-07-06 14:19:13 -0400

    PASS

--- crates/irctext/src/clientmsgs/pass.rs
+++ crates/irctext/src/clientmsgs/pass.rs
@@ -1,18 +1,34 @@
 use super::{ClientMessage, ClientMessageError, ClientMessageParts};
-use crate::{Message, ParameterList, RawMessage, ToIrcLine, Verb};
+use crate::{FinalParam, Message, ParameterList, RawMessage, ToIrcLine, Verb};
+
+pub type Password = FinalParam;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Pass;
+pub struct Pass(Password);
+
+impl Pass {
+    pub fn new(password: Password) -> Pass {
+        Pass(password)
+    }
+
+    pub fn password(&self) -> &Password {
+        &self.0
+    }
+
+    pub fn into_password(self) -> Password {
+        self.0
+    }
+}
 
 impl ClientMessageParts for Pass {
     fn into_parts(self) -> (Verb, ParameterList) {
-        todo!()
+        (Verb::Pass, ParameterList::builder().with_final(self.0))
     }
 }
 
 impl ToIrcLine for Pass {
     fn to_irc_line(&self) -> String {
-        todo!()
+        format!("PASS :{}", self.0)
     }
 }
 
@@ -32,6 +48,7 @@ impl TryFrom<ParameterList> for Pass {
     type Error = ClientMessageError;
 
     fn try_from(params: ParameterList) -> Result<Pass, ClientMessageError> {
-        todo!()
+        let (p,) = params.try_into()?;
+        Ok(Pass(p))
     }
 }

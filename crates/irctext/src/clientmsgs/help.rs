02998e89781ee6cbb02edebc0399209524523ef4
John T. Wodder II <git@varonathe.org>
2025-07-06 16:33:37 -0400

    HELP

--- crates/irctext/src/clientmsgs/help.rs
+++ crates/irctext/src/clientmsgs/help.rs
@@ -1,18 +1,49 @@
 use super::{ClientMessage, ClientMessageError, ClientMessageParts};
-use crate::{Message, ParameterList, RawMessage, ToIrcLine, Verb};
+use crate::{FinalParam, Message, ParameterList, RawMessage, ToIrcLine, Verb};
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct Help;
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct Help {
+    subject: Option<FinalParam>,
+}
+
+impl Help {
+    pub fn new() -> Help {
+        Help { subject: None }
+    }
+
+    pub fn new_with_subject(subject: FinalParam) -> Help {
+        Help {
+            subject: Some(subject),
+        }
+    }
+
+    pub fn subject(&self) -> Option<&FinalParam> {
+        self.subject.as_ref()
+    }
+
+    pub fn into_subject(self) -> Option<FinalParam> {
+        self.subject
+    }
+}
 
 impl ClientMessageParts for Help {
     fn into_parts(self) -> (Verb, ParameterList) {
-        todo!()
+        (
+            Verb::Help,
+            ParameterList::builder().maybe_with_final(self.subject),
+        )
     }
 }
 
 impl ToIrcLine for Help {
     fn to_irc_line(&self) -> String {
-        todo!()
+        let mut s = String::from("HELP");
+        if let Some(ref subject) = self.subject {
+            s.push(' ');
+            s.push(':');
+            s.push_str(subject.as_str());
+        }
+        s
     }
 }
 
@@ -32,6 +63,11 @@ impl TryFrom<ParameterList> for Help {
     type Error = ClientMessageError;
 
     fn try_from(params: ParameterList) -> Result<Help, ClientMessageError> {
-        todo!()
+        if params.is_empty() {
+            Ok(Help::new())
+        } else {
+            let (p,) = params.try_into()?;
+            Ok(Help::new_with_subject(p))
+        }
     }
 }

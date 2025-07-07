7c347804a44cae7bb465845c54d2eef587c939a4
John T. Wodder II <git@varonathe.org>
2025-07-06 21:16:36 -0400

    WHOWAS

--- crates/irctext/src/clientmsgs/whowas.rs
+++ crates/irctext/src/clientmsgs/whowas.rs
@@ -1,18 +1,52 @@
 use super::{ClientMessage, ClientMessageError, ClientMessageParts};
-use crate::{Message, ParameterList, RawMessage, ToIrcLine, Verb};
+use crate::util::DisplayMaybeFinal;
+use crate::{FinalParam, Message, Nickname, ParameterList, RawMessage, ToIrcLine, Verb};
+use std::num::NonZeroUsize;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub struct WhoWas;
+pub struct WhoWas {
+    nick: Nickname,
+    count: Option<NonZeroUsize>,
+}
+
+impl WhoWas {
+    pub fn new(nick: Nickname) -> WhoWas {
+        WhoWas { nick, count: None }
+    }
+
+    pub fn new_with_count(nick: Nickname, count: NonZeroUsize) -> WhoWas {
+        WhoWas {
+            nick,
+            count: Some(count),
+        }
+    }
+
+    pub fn nick(&self) -> &Nickname {
+        &self.nick
+    }
+
+    pub fn count(&self) -> Option<NonZeroUsize> {
+        self.count
+    }
+}
 
 impl ClientMessageParts for WhoWas {
     fn into_parts(self) -> (Verb, ParameterList) {
-        todo!()
+        let params = ParameterList::builder()
+            .with_medial(self.nick)
+            .maybe_with_final(self.count.map(|c| {
+                let Ok(param) = FinalParam::try_from(c.to_string()) else {
+                    unreachable!("A stringified integer should be a valid FinalParam");
+                };
+                param
+            }));
+        (Verb::WhoWas, params)
     }
 }
 
 impl ToIrcLine for WhoWas {
     fn to_irc_line(&self) -> String {
-        todo!()
+        format!("WHOWAS {}{}", self.nick, DisplayMaybeFinal(self.count))
     }
 }
 
@@ -32,6 +66,31 @@ impl TryFrom<ParameterList> for WhoWas {
     type Error = ClientMessageError;
 
     fn try_from(params: ParameterList) -> Result<WhoWas, ClientMessageError> {
-        todo!()
+        let (p1, p2): (_, Option<FinalParam>) = params.try_into()?;
+        let nick = match p1.as_str().parse::<Nickname>() {
+            Ok(nick) => nick,
+            Err(source) => {
+                return Err(ClientMessageError::ParseParam {
+                    index: 0,
+                    raw: p1.into_inner(),
+                    source: Box::new(source),
+                })
+            }
+        };
+        let count = if let Some(p) = p2 {
+            match p.as_str().parse::<NonZeroUsize>() {
+                Ok(count) => Some(count),
+                Err(source) => {
+                    return Err(ClientMessageError::ParseParam {
+                        index: 1,
+                        raw: p.into_inner(),
+                        source: Box::new(source),
+                    })
+                }
+            }
+        } else {
+            None
+        };
+        Ok(WhoWas { nick, count })
     }
 }

0405d7573034c145f0670b1d964ca0be04ec7548
John T. Wodder II <git@varonathe.org>
2025-07-14 08:26:25 -0400

    More methods for creating `AUTHENTICATE` messages

--- crates/irctext/src/clientmsgs/authenticate.rs
+++ crates/irctext/src/clientmsgs/authenticate.rs
@@ -1,5 +1,6 @@
 use super::{ClientMessage, ClientMessageError, ClientMessageParts};
 use crate::{FinalParam, Message, ParameterList, RawMessage, Verb};
+use base64::{Engine, engine::general_purpose::STANDARD};
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Authenticate {
@@ -9,6 +10,42 @@ pub struct Authenticate {
 impl Authenticate {
     pub fn new(parameter: FinalParam) -> Authenticate {
         Authenticate { parameter }
+    }
+
+    pub fn new_encoded(bytes: &[u8]) -> Vec<Authenticate> {
+        let mut b64 = STANDARD.encode(bytes);
+        let mut msgs = Vec::with_capacity(b64.len() / 400 + 1);
+        loop {
+            if b64.is_empty() {
+                let Ok(param) = "+".parse::<FinalParam>() else {
+                    unreachable!(r#""+" should be valid final param"#);
+                };
+                msgs.push(Authenticate::new(param));
+                return msgs;
+            } else {
+                let end = b64.len().min(400);
+                let Ok(param) = FinalParam::try_from(b64.drain(..end).collect::<String>()) else {
+                    unreachable!("base64 text should be valid final param");
+                };
+                msgs.push(Authenticate::new(param));
+            }
+        }
+    }
+
+    pub fn new_plain_sasl(
+        authorization: &str,
+        authentication: &str,
+        password: &str,
+    ) -> Vec<Authenticate> {
+        let s = format!("{authorization}\0{authentication}\0{password}");
+        Authenticate::new_encoded(s.as_ref())
+    }
+
+    pub fn new_abort() -> Authenticate {
+        let Ok(param) = "*".parse::<FinalParam>() else {
+            unreachable!(r#""*" should be valid final param"#);
+        };
+        Authenticate::new(param)
     }
 
     pub fn parameter(&self) -> &FinalParam {

81edefeff5797a9c57fc4d469b7084bc904dea35
John T. Wodder II <git@varonathe.org>
2026-01-17 13:03:45 -0500

    Update `ircnet` for tags

--- crates/ircnet/src/connect/consts.rs
+++ crates/ircnet/src/connect/consts.rs
@@ -5,3 +5,5 @@ pub const TLS_PORT: u16 = 6697;
 // Both RFC 2812 and <https://modern.ircdocs.horse> say that IRC messages (when
 // tags aren't involved) are limited to 512 characters, counting the CR LF.
 pub const MAX_LINE_LENGTH: usize = 512;
+
+pub const MAX_LINE_LENGTH_WITH_TAGS: usize = MAX_LINE_LENGTH + 8191;
